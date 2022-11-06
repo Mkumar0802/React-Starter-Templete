a133322dc40c232dbf65a94163cc8b59da54c20e
mkumar0802 <mkumar0802@gmail.com>
2022-11-06 19:55:13 +0530

    starter fix

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-fuchsia-500 ">
+      <h1 className="font-extrabold font-Poppins text-9xl ">Hello world!</h1>
+    </div>
+  );
+}
+
+
+
